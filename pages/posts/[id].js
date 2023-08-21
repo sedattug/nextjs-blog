3345d93c9aeb3c81e67b2b008b2cce5d6485b06f
unknown <sedattug@outlook.com>
2023-08-21 10:34:50 +0300

    Git files created

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    );
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        //If fallback is false, then any paths not returned by getStaticPaths will result in a 404
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
+}
